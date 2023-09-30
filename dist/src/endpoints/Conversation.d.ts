103d2c7f34cb706b36f2503fcd9a631b7f73311a
Ahsan Fazal <me@ahsanfazal.com>
2023-09-30 20:50:03 +0200

    Add ConversationEndpoint

---
+++ dist/src/endpoints/Conversation.d.ts
@@ -0,0 +1,251 @@
+import { components, operations } from "../lib/api";
+import { BaseEndpoint } from "./Base";
+export declare class ConversationEndpoint extends BaseEndpoint {
+    get(query: operations["getConversationCollection"]["parameters"]["query"]): Promise<import("openapi-fetch").FetchResponse<{
+        parameters: {
+            query?: {
+                _page?: number | undefined;
+                _items_per_page?: number | undefined;
+                _pagination?: boolean | undefined;
+            } | undefined;
+        };
+        responses: {
+            200: {
+                content: {
+                    "application/json": {
+                        shipment?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        shipmentQuotation?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    }[];
+                    "application/vnd.api+json": {
+                        shipment?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        shipmentQuotation?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    }[];
+                    "text/html": {
+                        shipment?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        shipmentQuotation?: {
+                            id?: string | undefined;
+                            sequence?: string | undefined;
+                        } | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    }[];
+                };
+            };
+        };
+    }>>;
+    getById(id: string): Promise<import("openapi-fetch").FetchResponse<{
+        parameters: {
+            path: {
+                id: string;
+            };
+        };
+        responses: {
+            200: {
+                content: {
+                    "application/json": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                    "application/vnd.api+json": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                    "text/html": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                };
+            };
+            404: {
+                content: {};
+            };
+        };
+    }>>;
+    post(body: components["schemas"]["Conversation-conversation-write"]): Promise<import("openapi-fetch").FetchResponse<{
+        requestBody?: {
+            content: {
+                "application/json": {
+                    relation?: string | undefined;
+                    shipment?: string | undefined;
+                    shipmentQuotation?: string | undefined;
+                    createdAt?: string | undefined;
+                    updatedAt?: string | undefined;
+                };
+                "application/vnd.api+json": {
+                    relation?: string | undefined;
+                    shipment?: string | undefined;
+                    shipmentQuotation?: string | undefined;
+                    createdAt?: string | undefined;
+                    updatedAt?: string | undefined;
+                };
+                "text/html": {
+                    relation?: string | undefined;
+                    shipment?: string | undefined;
+                    shipmentQuotation?: string | undefined;
+                    createdAt?: string | undefined;
+                    updatedAt?: string | undefined;
+                };
+            };
+        } | undefined;
+        responses: {
+            201: {
+                content: {
+                    "application/json": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                    "application/vnd.api+json": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                    "text/html": {
+                        shipment?: string | undefined;
+                        shipmentQuotation?: string | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        subject?: string | undefined;
+                        subjectType?: string | undefined;
+                    };
+                };
+            };
+            400: {
+                content: {};
+            };
+            404: {
+                content: {};
+            };
+            422: {
+                content: {};
+            };
+        };
+    }>>;
+    getMessages(id: string): Promise<import("openapi-fetch").FetchResponse<{
+        parameters: {
+            query?: {
+                _page?: number | undefined;
+                _items_per_page?: number | undefined;
+                _pagination?: boolean | undefined;
+            } | undefined;
+            path: {
+                id: string;
+            };
+        };
+        responses: {
+            200: {
+                content: {
+                    "application/json": {
+                        user?: string | undefined;
+                        employee?: string | undefined;
+                        conversation?: string | undefined;
+                        internal?: boolean | undefined;
+                        text: string;
+                        readByUser?: boolean | undefined;
+                        readByEmployee?: boolean | undefined;
+                        notificationSent?: boolean | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        excerpt?: string | undefined;
+                        relationPaths?: readonly string[] | undefined;
+                        idAsString?: string | undefined;
+                        idAsBytes?: string | undefined;
+                        idAsHex?: string | undefined;
+                        idAsDatabaseValue?: string | undefined;
+                    }[];
+                    "application/vnd.api+json": {
+                        user?: string | undefined;
+                        employee?: string | undefined;
+                        conversation?: string | undefined;
+                        internal?: boolean | undefined;
+                        text: string;
+                        readByUser?: boolean | undefined;
+                        readByEmployee?: boolean | undefined;
+                        notificationSent?: boolean | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        excerpt?: string | undefined;
+                        relationPaths?: readonly string[] | undefined;
+                        idAsString?: string | undefined;
+                        idAsBytes?: string | undefined;
+                        idAsHex?: string | undefined;
+                        idAsDatabaseValue?: string | undefined;
+                    }[];
+                    "text/html": {
+                        user?: string | undefined;
+                        employee?: string | undefined;
+                        conversation?: string | undefined;
+                        internal?: boolean | undefined;
+                        text: string;
+                        readByUser?: boolean | undefined;
+                        readByEmployee?: boolean | undefined;
+                        notificationSent?: boolean | undefined;
+                        id?: string | undefined;
+                        createdAt?: string | undefined;
+                        updatedAt?: string | undefined;
+                        excerpt?: string | undefined;
+                        relationPaths?: readonly string[] | undefined;
+                        idAsString?: string | undefined;
+                        idAsBytes?: string | undefined;
+                        idAsHex?: string | undefined;
+                        idAsDatabaseValue?: string | undefined;
+                    }[];
+                };
+            };
+            404: {
+                content: {};
+            };
+        };
+    }>>;
+}
--- dist/src/endpoints/Conversation.js
+++ dist/src/endpoints/Conversation.js
@@ -1,1 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ConversationEndpoint = void 0;
+const Base_1 = require("./Base");
+class ConversationEndpoint extends Base_1.BaseEndpoint {
+    get(query) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.httpClient.GET("/conversations", { params: { query } });
+        });
+    }
+    getById(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.httpClient.GET(`/conversations/{id}`, {
+                params: { path: { id } }
+            });
+        });
+    }
+    post(body) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.httpClient.POST("/conversations", { body });
+        });
+    }
+    getMessages(id) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return this.httpClient.GET(`/conversations/{id}/messages`, {
+                params: { path: { id } }
+            });
+        });
+    }
+}
+exports.ConversationEndpoint = ConversationEndpoint;
