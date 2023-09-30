bc9af81923ee756b98401ea99c82e0a96778af08
Ahsan Fazal <me@ahsanfazal.com>
2023-09-30 20:50:14 +0200

    Add CountryEndpoint

---
+++ src/endpoints/Country.ts
@@ -0,0 +1,16 @@
+import { operations } from "../lib/api"
+import { BaseEndpoint } from "./Base"
+
+export class CountryEndpoint extends BaseEndpoint {
+  public async get(
+    query: operations["getCountryCollection"]["parameters"]["query"]
+  ) {
+    return this.httpClient.GET("/countries", { params: { query } })
+  }
+
+  public async getById(id: string) {
+    return this.httpClient.GET(`/countries/{id}`, {
+      params: { path: { id } }
+    })
+  }
+}
