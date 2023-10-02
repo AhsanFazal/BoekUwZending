9a9a2598d2bf6f7a39004fc4538f46aef51704ae
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:35:29 +0200

    Add `Transport.Postcodes` endpoint

--- src/endpoints/Transport/endpoints/Postcodes.ts
+++ src/endpoints/Transport/endpoints/Postcodes.ts
@@ -1,3 +1,30 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Postcodes extends BaseEndpoint {}
+export class Postcodes extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/postcodes", { params: { query } })
+  }
+
+  public async create(body: any) {
+    return this.httpClient.POST("/transport/postcodes", { body })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/postcodes/{id}`, {
+      params: { path: { id } }
+    })
+  }
+
+  public async updateById(id: any, body: any) {
+    return this.httpClient.PUT(`/transport/postcodes/{id}`, {
+      params: { path: { id } },
+      body
+    })
+  }
+
+  public async deleteById(id: any) {
+    return this.httpClient.DELETE(`/transport/postcodes/{id}`, {
+      params: { path: { id } }
+    })
+  }
+}
