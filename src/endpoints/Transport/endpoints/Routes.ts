15f7c244a9e42510627ff5d074c630fe3a6d3d77
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:35:54 +0200

    Add `Transport.Routes` endpoint

--- src/endpoints/Transport/endpoints/Routes.ts
+++ src/endpoints/Transport/endpoints/Routes.ts
@@ -1,4 +1,44 @@
 import { BaseEndpoint } from "../../Base"
-import { components, operations } from "../../../lib/api"
 
-export class Routes extends BaseEndpoint {}
+export class Routes extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/routes", { params: { query } })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/routes/{id}`, {
+      params: { path: { id } }
+    })
+  }
+
+  public async updateById(id: any, body: any) {
+    return this.httpClient.PUT(`/transport/routes/{id}`, {
+      params: { path: { id } },
+      body
+    })
+  }
+
+  public async getStepsById(id: any) {
+    return this.httpClient.GET(`/transport/routes/{id}/steps`, {
+      params: { path: { id } }
+    })
+  }
+
+  public collections = {
+    get: (query: any) =>
+      this.httpClient.GET("/transport/route_collections", {
+        params: { query }
+      }),
+    getById: (id: any) =>
+      this.httpClient.GET(`/transport/route_collections/{id}`, {
+        params: { path: { id } }
+      }),
+    create: (body: any) =>
+      this.httpClient.POST("/transport/route_collections", { body }),
+    updateById: (id: any, body: any) =>
+      this.httpClient.PUT(`/transport/route_collections/{id}`, {
+        params: { path: { id } },
+        body
+      })
+  }
+}
