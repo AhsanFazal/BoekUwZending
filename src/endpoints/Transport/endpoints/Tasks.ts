4313ca8f2e4a4d82edca3429146e72efb9186fb5
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:36:17 +0200

    Add `Transport.Tasks` endpoint

--- src/endpoints/Transport/endpoints/Tasks.ts
+++ src/endpoints/Transport/endpoints/Tasks.ts
@@ -1,3 +1,63 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Tasks extends BaseEndpoint {}
+export class Tasks extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/tasks", { params: { query } })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/tasks/{id}`, {
+      params: { path: { id } }
+    })
+  }
+
+  public async updateById(id: any, body: any) {
+    return this.httpClient.PUT(`/transport/tasks/{id}`, {
+      params: { path: { id } },
+      body
+    })
+  }
+
+  public activities = {
+    get: (query: any) =>
+      this.httpClient.GET("/transport/task_activities", {
+        params: { query }
+      }),
+    getById: (id: any) =>
+      this.httpClient.GET(`/transport/task_activities/{id}`, {
+        params: { path: { id } }
+      })
+  }
+
+  public labels = {
+    get: (query: any) =>
+      this.httpClient.GET("/transport/task_labels", {
+        params: { query }
+      }),
+    getById: (id: any) =>
+      this.httpClient.GET(`/transport/task_labels/{id}`, {
+        params: { path: { id } }
+      }),
+    updateById: (id: any, body: any) =>
+      this.httpClient.PUT(`/transport/task_labels/{id}`, {
+        params: { path: { id } },
+        body
+      })
+  }
+
+  public pickups = {
+    get: (query: any) =>
+      this.httpClient.GET("/transport/task_pickups", {
+        params: { query }
+      }),
+    getById: (id: any) =>
+      this.httpClient.GET(`/transport/task_pickups/{id}`, {
+        params: { path: { id } }
+      }),
+    updateById: (id: any, body: any) =>
+      this.httpClient.PUT(`/transport/task_pickups/{id}`, {
+        params: { path: { id } },
+        body
+      })
+  }
+}
