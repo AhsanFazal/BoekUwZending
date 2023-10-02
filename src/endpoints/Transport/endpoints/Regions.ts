a8d862a8ead62ce026207389193119b8454d7af0
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:35:47 +0200

    Add `Transport.Regions` endpoint

--- src/endpoints/Transport/endpoints/Regions.ts
+++ src/endpoints/Transport/endpoints/Regions.ts
@@ -1,3 +1,24 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Regions extends BaseEndpoint {}
+export class Regions extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/regions", { params: { query } })
+  }
+
+  public async create(body: any) {
+    return this.httpClient.POST("/transport/regions", { body })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/regions/{id}`, {
+      params: { path: { id } }
+    })
+  }
+
+  public async updateById(id: any, body: any) {
+    return this.httpClient.PUT(`/transport/regions/{id}`, {
+      params: { path: { id } },
+      body
+    })
+  }
+}
