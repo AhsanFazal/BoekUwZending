3f43f3c7a5ccd976dfdac244b727db8205d1d1b3
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:36:03 +0200

    Add `Transport.Statuses` endpoint

--- src/endpoints/Transport/endpoints/Statuses.ts
+++ src/endpoints/Transport/endpoints/Statuses.ts
@@ -1,3 +1,17 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Statuses extends BaseEndpoint {}
+export class Statuses extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/statuses", { params: { query } })
+  }
+
+  public async create(body: any) {
+    return this.httpClient.POST("/transport/statuses", { body })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/statuses/{id}`, {
+      params: { path: { id } }
+    })
+  }
+}
