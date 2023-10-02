6ff2cea0a09f7eed3b41d256b8b5f26efede8b48
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:36:10 +0200

    Add `Transport.Steps` endpoint

--- src/endpoints/Transport/endpoints/Steps.ts
+++ src/endpoints/Transport/endpoints/Steps.ts
@@ -1,3 +1,19 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Steps extends BaseEndpoint {}
+export class Steps extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/steps", { params: { query } })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/steps/{id}`, {
+      params: { path: { id } }
+    })
+  }
+
+  public async deleteById(id: any) {
+    return this.httpClient.DELETE(`/transport/steps/{id}`, {
+      params: { path: { id } }
+    })
+  }
+}
