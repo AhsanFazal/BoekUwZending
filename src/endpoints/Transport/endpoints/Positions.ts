7f10ca7bd75052f3a3c6f587f927f5feab0bcdd7
Ahsan Fazal <me@ahsanfazal.com>
2023-10-02 09:06:24 +0200

    Add `Positions` endpoint

--- src/endpoints/Transport/endpoints/Positions.ts
+++ src/endpoints/Transport/endpoints/Positions.ts
@@ -1,3 +1,17 @@
 import { BaseEndpoint } from "../../Base"
 
-export class Positions extends BaseEndpoint {}
+export class Positions extends BaseEndpoint {
+  public async get(query: any) {
+    return this.httpClient.GET("/transport/positions", { params: { query } })
+  }
+
+  public async create(body: any) {
+    return this.httpClient.POST("/transport/positions", { body })
+  }
+
+  public async getById(id: any) {
+    return this.httpClient.GET(`/transport/positions/{id}`, {
+      params: { path: { id } }
+    })
+  }
+}
